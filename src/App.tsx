c12696e2d0e19179481b917e928798ce135b3e90
Samuel <samuel.montes@meltstudio.com>
2026-04-03 01:01:02 -0500

    feat: remove projects section and add improvements.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Header } from './components/General';
-import { Facts, Experience, Projects, Slider } from './components/body';
+import { Facts, Experience, Slider } from './components/body';
 import './App.css'
 function App() {
 
@@ -10,7 +10,6 @@ function App() {
       <Header />
       <Facts />
       <Experience />
-      <Projects />
       <Slider />
       <footer className="flex p-20 dark:text-secondary-dark text-secondary-light tiny:text-sm tiny:px-2 text-center  ">
         <span>Made with love ❤ by Samuel Montes</span>
